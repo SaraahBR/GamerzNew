157525c34972a4cdf4c978d75976812b19f598c3
Sarah Hernandes <vihernandesbr@gmail.com>
2025-08-26 13:19:29 -0300

    feat: Primeiro Projeto Angular GamerzNew

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { JogosComponent } from './pages/jogos/jogos.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'jogos', component: JogosComponent },
+  { path: 'sobre', component: SobreComponent },
+  { path: '**', redirectTo: '' }
+];
---
+++ src/app/pages/jogos/jogos.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jogos',
+  standalone: true,
+  templateUrl: './jogos.component.html',
+  styleUrls: ['./jogos.component.css']
+})
+export class JogosComponent {}
