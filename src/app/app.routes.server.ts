a69c157b197c152f95a419960556f41112c38ce2
Sarah Hernandes <vihernandesbr@gmail.com>
2025-08-26 16:17:45 -0300

    fix: Erro do Vercel de parâmetros.

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,17 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { GAMES } from './pages/jogos/games.data';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'jogos/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      return GAMES.map(g => ({ id: String(g.id) }));
+    },
+  },
+
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
